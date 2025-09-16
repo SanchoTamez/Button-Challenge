4ca6a3bd3e3850bc5e59cbc45078a99210c93457
Elan Tamez <tamezapple@gmail.com>
2025-09-16 15:08:10 -0600

    Initial Commit

---
+++ Button Challenge/Button_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Button_ChallengeApp.swift
+//  Button Challenge
+//
+//  Created by Tamez, Elan - Student on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Button_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
