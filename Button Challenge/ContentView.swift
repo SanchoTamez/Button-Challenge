972d598606fc10ac285a6ba4c24d29cc8eccd3fd
Elan Tamez <tamezapple@gmail.com>
2025-09-17 13:46:45 -0600

    final commitnment

--- Button Challenge/ContentView.swift
+++ Button Challenge/ContentView.swift
@@ -8,17 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter1 = 0
+    @State private var counter2 = 0
+    @State private var counter3 = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(counter1+counter2+counter3)")
+                .font(.system(size: 200))
+                .bold(true)
+                .foregroundStyle(.black)
+            
+            HStack {
+                
+                
+                
+                
+                Button {
+                    counter1 += 1
+                } label: {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(.blue)
+                        .overlay {
+                            Text("\(counter1)")
+                                .font(.system(size: 50))
+                                .bold(true)
+                                .foregroundStyle(.white)
+                        }
+                }
+                Button {
+                    counter2 += 2
+                } label: {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(.green)
+                        .overlay {
+                            Text("\(counter2)")
+                                .font(.system(size: 50))
+                                .bold(true)
+                                .foregroundStyle(.white)
+                        }
+                }
+                Button {
+                    counter3 += 3
+                } label: {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(.red)
+                        .overlay {
+                            Text("\(counter3)")
+                                .font(.system(size: 50))
+                                .bold(true)
+                                .foregroundStyle(.white)
+                        }
+                }
+                
+                
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
